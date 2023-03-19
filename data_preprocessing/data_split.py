fa837ffeef104d9b82694f3b5117ab30fce90066
Charles Wang <charles.w.developer@gmail.com>
2023-03-19 11:37:28 -0700

    Fix data split script paths

--- data_preprocessing/data_split.py
+++ data_preprocessing/data_split.py
@@ -1,15 +1,15 @@
 import os
 
 # Open the large JSON file
-with open('../data/yelp_academic_dataset_review.json', 'r', encoding='utf-8') as infile:
+with open('./data/yelp_academic_dataset_review.json', 'r', encoding='utf-8') as infile:
     # Set the maximum number of lines per chunk
     max_lines_per_chunk = 500000
     
     # Initialize the chunk counter
     chunk_num = 1
 
-    if not os.path.exists('../data/chunks'):
-        os.makedirs('../data/chunks')
+    if not os.path.exists('./data/chunks'):
+        os.makedirs('./data/chunks')
     
     # Read the file in chunks and write each chunk to a separate file
     while True:
@@ -26,7 +26,7 @@ with open('../data/yelp_academic_dataset_review.json', 'r', encoding='utf-8') as
             break
 
         # Create a new file for the chunk
-        chunk_file = f'../data/chunks/chunk_{chunk_num}.json'
+        chunk_file = f'./data/chunks/chunk_{chunk_num}.json'
         with open(chunk_file, 'w', encoding='utf-8') as outfile:
             # Write the lines to the chunk file
             outfile.writelines(lines)
